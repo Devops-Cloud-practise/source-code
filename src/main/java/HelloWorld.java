7caa6aa2f7186f97c6bb246e91e5544e0331d3e1
cigeek <diwshilphb82@gmail.com>
2017-11-07 15:27:23 +0530

    adding to UAT Branch

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World this is INT Branch");
+		get("/hello", (req, res) -> "Hello World this is UAT Branch");
 	}
 }
