c3cb549e9b11e3ccd663afda3f8e0955e2dde5f8
cigeek <diwshilphb82@gmail.com>
2017-11-07 15:44:36 +0530

    aas

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World this is Sprint Branch");
+		get("/hello", (req, res) -> "Hello WorlD this is Sprint Branch");
 	}
 }
